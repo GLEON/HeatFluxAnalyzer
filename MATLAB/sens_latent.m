337adfa7ba8a8ca9cc4423f0a785c6fa45d3e12f
riwoolway <riwoolway@gmail.com>
2014-08-26 20:12:38 +0100

    altered multiplication

--- MATLAB/sens_latent.m
+++ MATLAB/sens_latent.m
@@ -208,7 +208,7 @@ Uz(Uz < 0.2) = 0.2;
         % calculate tau and sensible and latent heat fluxes
         tau = C_D.*rho_a.*ustar.*ustar;
         ash = -rho_a.*const_SpecificHeatAir.*ustar.*tstar;           
-        alh = -rho_a.*xlv.*ustar*qstar;
+        alh = -rho_a.*xlv.*ustar.*qstar;
                 
         % calculate transfer coefficients at 10m
         C_H10 = ash./(rho_a.*const_SpecificHeatAir.*u10.*(ts - t10));
